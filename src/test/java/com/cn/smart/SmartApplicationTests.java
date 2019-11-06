b1dc456073654dab1edf1186fa3857b5484a106f
徐伟 <xuw@253.com>
2019-11-06 13:45:39 +0800

    首次提交，创建项目架构

---
+++ src/test/java/com/cn/smart/SmartApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cn.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SmartApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
