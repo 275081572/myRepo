f9967034d89854b15cff1a8c76e5c56c0af1acad
xuwei <275081572@qq.com>
2020-07-17 08:48:33 +0800

    测试文件压缩

--- src/main/java/com/cn/smart/controller/user/UserController.java
+++ src/main/java/com/cn/smart/controller/user/UserController.java
@@ -1,8 +1,10 @@
 package com.cn.smart.controller.user;
 
 import com.cn.smart.common.JsonResult;
+import com.cn.smart.dto.FileBean;
 import com.cn.smart.jpa.entity.user.User;
 import com.cn.smart.service.user.IUserService;
+import com.cn.smart.util.ZipUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -16,6 +18,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/api/1.0/user")
@@ -76,5 +88,51 @@ public class UserController {
         userService.ececptionTest();
         return JsonResult.ok();
     }
+
+    /**
+     * 打包压缩下载文件
+     */
+    @RequestMapping(value = "/downLoadZipFile")
+    public void downLoadZipFile(HttpServletResponse response) throws IOException {
+        String zipName = "myfile.zip";
+        //构建测试数据
+        List<FileBean> fileList = new ArrayList<>(2);
+        FileBean fileBeanA = new FileBean();
+        fileBeanA.setFilePath("E:/个人文档/");
+        fileBeanA.setFileName("王维维-JAVA-五年.docx");
+
+        FileBean fileBeanB = new FileBean();
+        fileBeanB.setFilePath("E:/个人文档/");
+        fileBeanB.setFileName("测试.txt");
+
+        fileList.add(fileBeanA);
+        fileList.add(fileBeanB);
+
+        response.setContentType("APPLICATION/OCTET-STREAM");
+        response.setHeader("Content-Disposition","attachment; filename="+zipName);
+        //跨域支持
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Method", "POST,GET");
+        ZipOutputStream out = new ZipOutputStream(response.getOutputStream());
+
+        try {
+            for(Iterator<FileBean> it = fileList.iterator(); it.hasNext();){
+                FileBean file = it.next();
+                //修改文件名
+                File f = new File(file.getFilePath()+file.getFileName());
+                File newFile = new File(file.getFilePath() + System.currentTimeMillis() +
+                        file.getFileName().substring(file.getFileName().lastIndexOf(".")));
+                f.renameTo(newFile);
+
+                ZipUtils.doCompress(newFile, out);
+                response.flushBuffer();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+    }
+
 
 }
---
+++ src/main/java/com/cn/smart/dto/FileBean.java
@@ -0,0 +1,16 @@
+package com.cn.smart.dto;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: xuwei
+ * @Date: 2020/7/8 9:14
+ */
+@Data
+public class FileBean {
+
+    private String filePath;
+
+    private String fileName;
+}
